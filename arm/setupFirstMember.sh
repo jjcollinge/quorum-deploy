dfda4549c23ee06a1541b00e19715aa446b90234
Jonathan Collinge <jonathancollinge@live.com>
2017-05-19 05:59:52 +0100

    Fixed sed

--- arm/setupFirstMember.sh
+++ arm/setupFirstMember.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 while getopts ":a:b:c:d:e:f:g:h:i:j:k:l:m" opt; do
-  case $opt in
+  case "$opt" in
     a) IsVoter="$OPTARG"
     ;;
     b) VoterAccountAddress="$OPTARG"
@@ -36,29 +36,30 @@ git clone https://github.com/jjcollinge/quorum-node --branch feature/registry
 cd quorum-node/source/
 
 # Inject geth config
-sed -i -e "s/__IsVoter__/$IsVoter/g" geth/config.json
-sed -i -e "s/__VoterAccountAddress__/$VoterAccountAddress/g" geth/config.json
-sed -i -e "s/__VoterAccountPassword__/$VoterAccountPassword/g" geth/config.json
-sed -i -e "s/__IsBlockmaker__/$isBlockmaker/g" geth/config.json
-sed -i -e "s/__BlockmakerAccountAddress__/$BlockmakerAccountAddress/g" geth/config.json
-sed -i -e "s/__BlockmakerAccountPassword__/$BlockmakerAccountPassword/g" geth/config.json
-sed -i -e "s/__GethNetworkId__/$GethNetworkId/g" geth/config.json
-sed -i -e "s/__AzureStorageConnectionString__/$AzureStorageConnectionString/g" geth/config.json
-sed -i -e "s/__AzureTenant__/$AzureTenant/g" geth/config.json
-sed -i -e "s/__AzureSPNAppId__/$AzureSPNAppId/g" geth/config.json
-sed -i -e "s/__AzureSPNPassword__/$AzureSPNPassword/g" geth/config.json
+sed -i -e 's/__IsVoter__/'"$IsVoter"'/g' geth/config.json
+sed -i -e 's/__VoterAccountAddress__/'"$VoterAccountAddress"'/g' geth/config.json
+sed -i -e 's/__VoterAccountPassword__/'"$VoterAccountPassword"'/g' geth/config.json
+sed -i -e 's/__IsBlockmaker__/'"$isBlockmaker"'/g' geth/config.json
+sed -i -e 's/__BlockmakerAccountAddress__/'"$BlockmakerAccountAddress"'/g' geth/config.json
+sed -i -e 's/__BlockmakerAccountPassword__/'"$BlockmakerAccountPassword"'/g' geth/config.json
+sed -i -e 's/__GethNetworkId__/'"$GethNetworkId"'/g' geth/config.json
+sed -i -e 's@__AzureStorageConnectionString__@'"$AzureStorageConnectionString"'@g' geth/config.json
+sed -i -e 's/__AzureTenant__/'"$AzureTenant"'/g' geth/config.json
+sed -i -e 's/__AzureSPNAppId__/'"$AzureSPNAppId"'/g' geth/config.json
+sed -i -e 's/__AzureSPNPassword__/'"$AzureSPNPassword"'/g' geth/config.json
 
 # Inject bootnode config
-sed -i -e "s/__ContainerHostIp__/$ContainerHostIp/g" bootnode/config.json
-sed -i -e "s/__GethNetworkId__/$GethNetworkId/g" bootnode/config.json
-sed -i -e "s/__AzureStorageConnectionString__/$AzureStorageConnectionString/g" bootnode/config.json
-sed -i -e "s/__AzureTenant__/$AzureTenant/g" bootnode/config.json
-sed -i -e "s/__AzureSPNAppId__/$AzureSPNAppId/g" bootnode/config.json
+sed -i -e 's/__ContainerHostIp__/'"$ContainerHostIp"'/g' bootnode/config.json
+sed -i -e 's/__GethNetworkId__/'"$GethNetworkId"'/g' bootnode/config.json
+sed -i -e 's@__AzureStorageConnectionString__@'"$AzureStorageConnectionString"'@g' bootnode/config.json
+sed -i -e 's/__AzureTenant__/'"$AzureTenant"'/g' bootnode/config.json
+sed -i -e 's/__AzureSPNAppId__/'"$AzureSPNAppId"'/g' bootnode/config.json
+sed -i -e 's/__AzureSPNPassword__/'"$AzureSPNPassword"'/g' bootnode/config.json
 
 # Inject constellation config
 sed -i -e "s/__OtherConstellationNodes__//g" constellation/node.conf
 
 # Inject cakeshop config
-sed -i -e "s/__GethNetworkId__/$GethNetworkId/g" quorum-bootnode.yml
+sed -i -e 's/__GethNetworkId__/'"$GethNetworkId"'/g' quorum-bootnode.yml
 
 docker-compose -f quorum-bootnode.yml up -d
