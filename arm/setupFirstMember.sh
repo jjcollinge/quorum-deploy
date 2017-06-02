e398f4de9c73a9c8e7ef4241eb5708376b8e053d
Jonathan Collinge <jonathancollinge@live.com>
2017-06-02 16:44:04 +0100

    Update init script

--- arm/setupFirstMember.sh
+++ arm/setupFirstMember.sh
@@ -19,9 +19,12 @@ while getopts ":a:b:c:d:e:f:g:" opt; do
   esac
 done
 
+BASEDIR=$(pwd)
+echo "Starting first member setup" | tee $BASEDIR/setup.log
+
 if [[ $(dpkg -l | grep az | wc -l) == 0 ]]; then
   # Install azure cli 2.0
-  echo "Installing Azure CLI 2.0" | tee setup.log
+  echo "Installing Azure CLI 2.0" | tee -a $BASEDIR/setup.log
   echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
       tee /etc/apt/sources.list.d/azure-cli.list
 
@@ -32,22 +35,22 @@ fi
 
 if [[ $(dpkg -l | grep unzip | wc -l) == 0 ]]; then
   # Install unzip
-  echo "Installing unzip" | tee setup.log
+  echo "Installing unzip" | tee -a $BASEDIR/setup.log
   apt-get install -y unzip
 fi
 
 # Clone the source from remote location
-echo "Cloning source repo" | tee setup.log
+echo "Cloning source repo" | tee -a $BASEDIR/setup.log
 cd /opt
 git clone https://github.com/jjcollinge/quorum-deploy
 cd quorum-deploy/source/
 
 # Fetch the geth files from blob
-echo "Fetching geth files from blob" | tee setup.log
+echo "Fetching geth files from blob" | tee -a $BASEDIR/setup.log
 az login --service-principal -u $AzureSPNAppId -p $AzureSPNPassword --tenant $AzureTenant
 az account set -s $AzureSubscriptionId
 
-echo "Downloading blob" | tee setup.log
+echo "Downloading blob" | tee -a $BASEDIR/setup.log
 export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string \
     --name $AzureBlobStorageName \
     --resource-group $AzureResourceGroup \
@@ -55,13 +58,14 @@ export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-stri
 az storage blob download -c node -n files.zip -f /opt/quorum-deploy/node.zip
 unzip /opt/quorum-deploy/node.zip -d /opt/quorum-deploy
 
-# Generate a sas token for table storage
-echo "Generating SAS token for table storage" | tee setup.log
+# Create the table and generate a sas token
+echo "Generating SAS token for table storage" | tee -a $BASEDIR/setup.log
 AzureTableStorageName=$AzureBlobStorageName
+az storage table create -n networkbootnodes | tee -a $BASEDIR/setup.log
 AzureTableStorageSas=$(az storage table generate-sas --name networkbootnodes --account-name $AzureBlobStorageName --permissions raud)
 
 # Inject table storage details if not provided (i.e. is firstMember)
-echo "Injecting values into config" | tee setup.log
+echo "Injecting values into config" | tee -a $BASEDIR/setup.log
 if ! grep -q "AzureTableStorageName" /opt/quorum-deploy/node/config.json; then
 python << END
 import json
@@ -79,7 +83,7 @@ echo "New config..."
 cat /opt/quorum-deploy/node/config.json
 
 # Copy files to local geth source
-echo "Copying files to local geth source" | tee setup.log
+echo "Copying files to local geth source" | tee -a $BASEDIR/setup.log
 cp /opt/quorum-deploy/node/genesis.json /opt/quorum-deploy/source/geth/
 mkdir -p /opt/quorum-deploy/source/geth/keys
 cp /opt/quorum-deploy/node/key* /opt/quorum-deploy/source/geth/keys
@@ -95,10 +99,10 @@ sed -i -e 's/__GethNetworkId__/'"$GethNetworkId"'/g' /opt/quorum-deploy/source/q
 
 # [Re]build docker images if desired
 if [[ "$Rebuild" = true ]]; then
-  echo "Building docker images" | tee setup.log
+  echo "Building docker images" | tee -a $BASEDIR/setup.log
   docker-compose -f /opt/quorum-deploy/source/quorum-bootnode.yml build
 fi
 
 # Bring up docker containers
-echo "Bringing up docker containers..." | tee setup.log
+echo "Bringing up docker containers..." | tee -a $BASEDIR/setup.log
 docker-compose -f /opt/quorum-deploy/source/quorum-bootnode.yml up -d
--- source/geth/start.sh
+++ source/geth/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Starting geth initialisation" | tee setup.log
+
 # Load config
-echo "Loading configuration file"| tee temp/logs/start.log
+echo "Loading configuration file" | tee -a temp/logs/start.log
 rm -f /opt/quorum/env.sh
 python /opt/quorum/loadconfig.py
 source /opt/quorum/env.sh
@@ -16,65 +18,68 @@ blob_container="node"
 blob_file="files.zip"
 
 if [[ -z $GETHNETWORKID ]]; then
-    echo "Empty or invalid required config.json field: GethNetworkId"| tee temp/logs/start.log
+    echo "Empty or invalid required config.json field: GethNetworkId" | tee -a temp/logs/start.log
     exit 1
 fi
 if [[ -z $AZURETENANT ]]; then
-    echo "Empty or invalid required config.json field: AzureTenant"| tee temp/logs/start.log
+    echo "Empty or invalid required config.json field: AzureTenant" | tee -a temp/logs/start.log
     exit 1
 fi
 if [[ -z $AZURESPNAPPID ]]; then
-    echo "Empty or invalid required config.json field: AzureSPNAppId"| tee temp/logs/start.log
+    echo "Empty or invalid required config.json field: AzureSPNAppId" | tee -a temp/logs/start.log
     exit 1
 fi
 if [[ -z $AZURESPNPASSWORD ]]; then
-    echo "Empty or invalid required config.json field: AzureSPNPassword"| tee temp/logs/start.log
+    echo "Empty or invalid required config.json field: AzureSPNPassword" | tee -a temp/logs/start.log
     exit 1
 fi
 if [[ -z $AZURESUBSCRIPTIONID ]]; then
-    echo "Empty or invalid required config.json field: AzureSubscriptionId"| tee temp/logs/start.log
+    echo "Empty or invalid required config.json field: AzureSubscriptionId" | tee -a temp/logs/start.log
     exit 1
 fi
 if [[ -z $AZURETABLESTORAGENAME ]]; then
-    echo "Empty or invalid required config.json field: AzureTableStorageName"| tee temp/logs/start.log
+    echo "Empty or invalid required config.json field: AzureTableStorageName" | tee -a temp/logs/start.log
     exit 1
 fi
 if [[ -z $AZURETABLESTORAGESAS ]]; then
-    echo "Empty or invalid required config.json field: AzureTableStorageSas"| tee temp/logs/start.log
+    echo "Empty or invalid required config.json field: AzureTableStorageSas" | tee -a temp/logs/start.log
     exit 1
 fi
 
 # Login to Azure Storage with SPN
-echo "Logging into Azure"| tee temp/logs/start.log
-az login --service-principal -u $AZURESPNAPPID -p $AZURESPNPASSWORD --tenant $AZURETENANT | tee temp/logs/start.log
-az account set -s $AZURESUBSCRIPTIONID | tee temp/logs/start.log
+echo "Logging into Azure" | tee -a temp/logs/start.log
+az login --service-principal -u $AZURESPNAPPID -p $AZURESPNPASSWORD --tenant $AZURETENANT | tee -a temp/logs/start.log
+az account set -s $AZURESUBSCRIPTIONID  | tee -a temp/logs/start.log
 
 # Initialise Geth
-echo "Initialising geth"| tee temp/logs/start.log
+echo "Initialising geth" | tee -a temp/logs/start.log
 geth --datadir /opt/quorum/data init genesis.json
 
 # Copy key files into keystore
-echo "Copying key files to keystore"| tee temp/logs/start.log
+echo "Copying key files to keystore" | tee -a temp/logs/start.log
 for key in "keys/key*"; do
     cp $key /opt/quorum/data/keystore
 done
 
 # Check bootnode registry exists
-echo "Checking whether bootnode registry '$azure_storage_table' exists"| tee temp/logs/start.log
+echo "Checking whether bootnode registry '$azure_storage_table' exists" | tee -a temp/logs/start.log
 table_args="--account-name $AZURETABLESTORAGENAME --sas-token $AZURETABLESTORAGESAS"
 exists=$(az storage table exists --name $azure_storage_table $table_args)
 
 if [[ $exists == *"true"* ]]; then
     # Fetch current value
-    echo "Fetching existing bootnodes from registry"| tee temp/logs/start.log
+    echo "Fetching existing bootnodes from registry" | tee -a temp/logs/start.log
     response=$(az storage entity show -t $azure_storage_table --partition-key $azure_partition_key --row-key $azure_row_key $table_args | grep -e "enode://" | awk '{ print $2 }')
     current_bootnodes=${response:1:-2}
-    echo "Current bootnodes: $current_bootnodes"| tee temp/logs/start.log
+    echo "Current bootnodes: $current_bootnodes" | tee -a temp/logs/start.log
+else
+    echo "Table storage should have been provisioned!" | tee -a temp/logs/start.log
+    exit 1
 fi
 
 if [[ -z $current_bootnodes ]]; then
     # Don't use bootnodes
-    echo "No existing bootnodes in registry"| tee temp/logs/start.log
+    echo "No existing bootnodes in registry" | tee -a temp/logs/start.log
     bootnode_args=""
 else
     # Use bootnodes
@@ -92,6 +97,6 @@ if [[ "${ISBLOCKMAKER,,}" = 'true' ]];then
     args="$args --blockmakeraccount $BLOCKMAKERACCOUNTADDRESS --blockmakerpassword \"${BLOCKMAKERACCOUNTPASSWORD}\" "
 fi
 
-echo "Starting geth with args: $args"| tee temp/logs/start.log
+echo "Starting geth with args: $args" | tee -a temp/logs/start.log
 PRIVATE_CONFIG=/opt/quorum/data/constellation.ipc
 eval geth "${args}" 2>>temp/logs/geth.log
